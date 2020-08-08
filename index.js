d35f7222ae343b2a68e5179d94ee2eb087c18335
Marzia Deodato <marziadeodato@gmail.com>
2020-08-08 16:55:50 +0200

    added getCookie function

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-  
+/**
+ * Making the fetch request to the URL_API
+ */
+
+
+const URL_API = "https://cfw-takehome.developers.workers.dev/api/variants"
+const COOKIE_NAME = "URL" 
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -11,3 +18,26 @@ async function handleRequest(request) {
     headers: { 'content-type': 'text/plain' },
   })
 }
+
+
+
+/**
+ * From the request it gets the name of the cookie.
+ * @param {Request} request 
+ * @param {string} name 
+ */
+function getCookie(request, name) {
+    let result = null
+    let cookieString = request.headers.get("Cookie")
+    if (cookieString) {
+      let cookies = cookieString.split(";")
+      cookies.forEach((cookie) => {
+        let cookieName = cookie.split("=")[0].trim()
+        if (cookieName === name) {
+          let cookieVal = cookie.split("=")[1]
+          result = cookieVal
+        }
+      })
+    }
+    return result
+  }
