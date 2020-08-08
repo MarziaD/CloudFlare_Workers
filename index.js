21ba807c93421377f67e3336b55cb5c539053fb1
Marzia Deodato <marziadeodato@gmail.com>
2020-08-08 17:13:52 +0200

    added fetchWebPage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 /**
- * Respond with hello worker text
+ * Respond with 
  * @param {Request} request
  */
 async function handleRequest(request) {
@@ -41,3 +41,44 @@ function getCookie(request, name) {
     }
     return result
   }
+
+/**
+ * Fetches a random web page from the urls listed in URL_API
+ * @param {Null | string} cookie
+ */
+
+async function fetchWebPage(cookie = null) {
+  const url = cookie ? cookie : await fetchURL()
+  const res = await fetch(url)
+
+  const titleHandler = new TitleHandler()
+  const descriptionHandler = new DescriptionHandler()
+  const ctaHandler = new CTAHandler()
+
+  const response = new HTMLRewriter()
+    .on("title", titleHandler)
+    .on("h1#title", titleHandler)
+    .on("p#description", descriptionHandler)
+    .on("a#url", ctaHandler)
+    .transform(res)
+
+  if (!cookie) {
+    response.headers.set("Set-Cookie", `${COOKIE_NAME}=${url}`)
+  }
+
+  return response
+}
+
+/**
+ * Fetches a random url from URL_API (https://cfw-takehome.developers.workers.dev/api/variants)
+ */
+async function fetchURL() {
+  const res = await fetch(URL_API)
+  const data = await res.json()
+  const urls = data.variants
+  const randomIndex = Math.round(Math.random())
+
+  return urls[randomIndex]
+}
+
+
